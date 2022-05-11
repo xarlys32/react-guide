b4993e55667ddb79dbfda40d9063f7dca5e871e2
xarlys32 <57732203+xarlys32@users.noreply.github.com>
2022-05-11 19:41:59 +0200

    Http

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import ExpenseItem from "./components/ExpenseItem";
-const hello = () => {
-  return "oooW MEEN"
-};
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const fetchMovieHandler = () => {
+    setIsLoading(true);
+    setError(null);
+    fetch('https://swapi.dev/api/films/').then((response) => {
+      //transform the response to a json
+      if(!response.ok) {
+        throw new Error('Ups, We found a mistake!')
+      }
+
+      return response.json();
+    }).then((data) => {
+      //once the response is transformed
+      const moviesList = data.results.map(item => {
+        return {
+          id: item.episode_id,
+          title: item.title,
+          openingText: item.opening_crawl,
+          releaseDate: item.release_date
+        }
+      });
+      setMovies(moviesList)
+      setIsLoading(false);
+    }).catch((errordata) => {
+      setError(errordata.message)
+    }).finally(() => {
+      setIsLoading(false);
+    });
+  }
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <p>Ñee {hello}</p>
-      <ExpenseItem></ExpenseItem>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {error && <p>{error}</p>}
+        {!isLoading && movies.length > 0 && !error && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading..</p>}
+        {!isLoading && movies.length === 0 && !error &&<p>No movies found</p>}
+      </section>
+    </React.Fragment>
   );
 }
 
